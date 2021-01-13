bb4e8d613229b86831cc2237d4deff9b98a61f56
LCS Game Design <tom.shiflet@mylcs.com>
2021-01-13 07:46:01 -0600

    Initial Commit

---
+++ SpaceShooter/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpaceShooter
+//
+//  Created by Tom on 1/13/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
